b8b826eb44fc0aded64ce640e02ba22078b2f2f2
LukeButterworth <luke.butterworth@protonmail.com>
2023-08-06 00:03:03 +1000

    Added support for verbose and fuzzy flags

--- src/main.c
+++ src/main.c
@@ -22,18 +22,37 @@ int main (int argc, char* argv[]) {
 		exit(0);
 	}
 
+	bool is_verbose = false;
+	bool is_fuzzy = false;
+	for (int i = 1; i < argc; i++) {
+		char* arg = argv[i];
+		is_verbose = is_verbose || !strcmp(arg, "-v");
+		is_fuzzy = is_fuzzy || !strcmp(arg, "-f");
+	}
+
+	if (is_verbose) printf("Running in verbose mode.\n");
+	if (is_fuzzy) printf("Using fuzzy matching.\n");
+
 	for (int i = 1; i < argc; i++) {
 		char* arg = argv[i];
 		if (!is_valid_arg(arg)) {
 			printf(INVALID_ARG_ERR, *arg);
 			continue;
 		}
-		printf("Attempting to convert %s.\n", arg);
+
+		int value = convert_arg(arg);
+		printf("Converted %s to %d.\n", arg, value);
 	}
 
 	exit(0);
 }
 
 bool is_valid_arg(char* arg) {
+	//test characters
+	//test structure
 	return true;
+}
+
+int convert_arg(char* arg) {
+	return 0;
 }
