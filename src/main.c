5536eeb79c6e4d099af35fff7263be95633c81d8
LukeButterworth <luke.butterworth@protonmail.com>
2023-08-05 21:47:26 +1000

    Added argument check to program

--- src/main.c
+++ src/main.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include <string.h>
 
-int main (int argc, int* argv[]) {
+int main (int argc, char* argv[]) {
+	printf("Testing\n");
+	if (!argc) {
+		printf("No argument provided.\n");
+		exit(0);
+	}
 	exit(0);
 }
