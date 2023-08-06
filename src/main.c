152354a83eb8c09798c794bc86a38fc3aec2f193
LukeButterworth <luke.butterworth@protonmail.com>
2023-08-06 18:23:01 +1000

    Filtered out tags on main loop

--- src/main.c
+++ src/main.c
@@ -6,13 +6,15 @@
 //String Literal Definitions
 #define NO_ARG_ERR "No argument provided.\n"
 #define INVALID_ARG_ERR "Argument %s is not a valid roman numeral.\n"
+#define VERBOSE_MSG "Running in verbose mode.\n"
+#define FUZZY_MSG "Using fuzzy matching.\n"
+#define RESULT_MSG "Converted %s to %d.\n"
 
 //Constant Definitions
 const char* valid_numerals = "IVXLCDM";
 
 //Function Declarations
 bool is_valid_arg(char* arg);
-
 int convert_arg(char* arg);
 
 //Main Function
@@ -35,18 +37,24 @@ int main (int argc, char* argv[]) {
 		is_fuzzy = is_fuzzy || has_vf_flag || has_f_flag;
 	}
 
-	if (is_verbose) printf("Running in verbose mode.\n");
-	if (is_fuzzy) printf("Using fuzzy matching.\n");
+	if (is_verbose) printf(VERBOSE_MSG);
+	if (is_verbose && is_fuzzy) printf(FUZZY_MSG);
 
 	for (int i = 1; i < argc; i++) {
 		char* arg = argv[i];
+		if (arg[0] == '-') continue;
+
 		if (!is_valid_arg(arg)) {
 			printf(INVALID_ARG_ERR, *arg);
 			continue;
 		}
 
 		int value = convert_arg(arg);
-		printf("Converted %s to %d.\n", arg, value);
+		if (is_verbose) {
+			printf(RESULT_MSG, arg, value);
+		} else {
+			printf("%d", value);
+		}
 	}
 
 	exit(0);
